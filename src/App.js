193f6409edd5c4618ec8dda97283eb7d9b3c13e7
Anna Maloy <alscarbo@ksu.edu>
2019-07-17 14:06:30 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+  let sessions = [
+    "React",
+    "C#",
+    "Python"
+  ];
+
+  return ( 
+  <React.Fragment>
+    <h1>KCDC Sessions</h1>
+      {sessions.map(session => <li>{session}</li>)}
+  </React.Fragment>
+    );
 }
 
 export default App;
