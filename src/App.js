1614b0f04940b019db3d57d9dfd3ada3b0b9961a
Anna Maloy <alscarbo@ksu.edu>
2019-07-17 14:27:43 -0500

    Update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React from 'react';
+import React, { useState } from "react";
 
-function App(){
-  let sessions = [
-    "React",
-    "C#",
-    "Python"
-  ];
+function App() {
+  let sessions = ["React", "C#", "Python"];
 
-  return ( 
-  <React.Fragment>
-    <h1>KCDC Sessions</h1>
-      {sessions.map(session => <li>{session}</li>)}
-  </React.Fragment>
-    );
+  function renderSession(session) {
+    return <li key={session}>{session}</li>;
+  }
+
+  return (
+    <>
+      <h1>KCDC Sessions</h1>
+      <ul>{sessions.map(renderSession)}</ul>
+    </>
+  );
 }
 
 export default App;
